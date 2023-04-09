8d7f9401c80da3968d8cf13b36edb6016d4c2cc2
Maksat Kuanysh <boktaban@gmail.com>
2023-04-09 18:14:54 +0600

    feat: add constants

---
+++ constants/constants.js
@@ -0,0 +1,3 @@
+export const MULTIPLY_CHAR = 'x'
+// TODO: change city name from nur-sultan
+export const TOP_PRODUCTS_BASE_URL = 'https://kaspi.kz/shop/nur-sultan/c/food/?q=%3Acategory%3AFood%3Arating%3A3-5%20stars&sort=relevance'
---
+++ constants/providers.constants.js
@@ -0,0 +1,8 @@
+// need to test another urls
+export const KAZAKHTELECOM = 'kazakhtelecom' // (https://consumer.oofd.kz/ticket/653bf166-a399-4260-9557-4f6eaf3682b8)
+export const TTC = 'transtelecom' // (https://ofd1.kz/t/?i=56689177603&f=010101907419&s=2508.0&t=20221216T152000)
+
+export const providers = {
+	'ofd1.kz': TTC,
+	'consumer.oofd.kz': KAZAKHTELECOM,
+}
