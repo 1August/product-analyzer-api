9546b881de1ba1bc5acf307f35734c196d73d2a0
Maksat Kuanysh <boktaban@gmail.com>
2023-05-14 12:26:36 +0600

    feat: data routes

--- routes/data.routes.js
+++ routes/data.routes.js
@@ -1,33 +1,23 @@
 import { Router } from 'express'
-import puppeteer from 'puppeteer'
+import { launch } from 'puppeteer'
 import { Supermarket } from '../models/Supermarket.js'
 import { TOP_PRODUCTS_BASE_URL } from '../constants/constants.js'
+import { getHref, getInnerText, getSrc } from '../utils/puppeteer.utils.js'
+import { removeNewLineFromString, trimBoth } from '../utils/string.js'
 
 const router = Router()
 
-async function getPropertyOfProduct(product, propName) {
-	return (await product.getProperty(propName)).jsonValue()
-}
-
-async function getTextOfProduct(product, selector) {
-	const productPromise = await getPropertyOfProduct(await product.$(selector), 'innerText')
-	const productContent = await productPromise
-	return productContent
-}
-
 async function getInfoOfProduct(product) {
 	// Should be like a Product model in mongodb/mongoose
-	const productId = await getPropertyOfProduct(product, 'href')
-	const productName = await getTextOfProduct(product, '.product-block__descr')
-	const productPrice = await getTextOfProduct(product, '.product-block__price')
+	const productId = await getHref(product)
+	const productName = await getInnerText(product.$('.product-block__descr'))
+	const productPrice = await getInnerText(product.$('.product-block__price'))
 
-	const productInfo = {
+	return {
 		id: productId.substring(productId.lastIndexOf('/') + 1, productId.indexOf('?')),
 		name: productName,
 		price: productPrice,
 	}
-
-	return productInfo
 }
 
 // /api/data/scrapProducts
@@ -39,7 +29,7 @@ router.get('/scrapProducts', async (req, res) => {
 	const products = new Map()
 
 	try {
-		const browser = await puppeteer.launch()
+		const browser = await launch()
 		const page = await browser.newPage()
 		await page.goto(market.productsPageUrl)
 
@@ -66,31 +56,44 @@ router.get('/top', async (req, res) => {
 	try {
 		const products = []
 
-		const browser = await puppeteer.launch()
+		const browser = await launch()
 		const page = await browser.newPage()
+
+		// const URLs = []
+		// for (let i = 1; i <= NUMBER_OF_TOP_PRODUCT_PAGES_TO_SCRAP; i++) {
+		// 	const url = `${TOP_PRODUCTS_BASE_URL}?page=${i}`
+		// 	URLs.push(url)
+		// }
+
+		// for (const url of URLs) {
 		await page.goto(TOP_PRODUCTS_BASE_URL)
 
-		await page.waitForSelector('.item-cards-grid').then(async () => {
-			const ids = await page.$$eval('.item-card', products =>
-				products.reduce((res, product) => [
-					...res,
-					product.getAttribute('data-product-id'),
-				], []))
-
-			const productBlocks = await page.$$('.item-card')
-
-			for (let i = 0; i < productBlocks.length; i++) {
-				const nameSelector = await productBlocks[i].$('.item-card__name-link')
-				const priceSelector = await productBlocks[i].$('.item-card__prices-price')
-
-				const productInfo = {
-					id: ids[i],
-					name: await getPropertyOfProduct(nameSelector, 'innerText'),
-					price: (await getPropertyOfProduct(priceSelector, 'innerText')),
-				}
-				products.push(productInfo)
-			}
+		await page.waitForSelector('.item-cards-grid')
+		await page.waitForSelector('.item-card:nth-of-type(12)', {
+			visible: true,
 		})
+
+		const productBlocks = await page.$$('.item-card')
+
+		for (let i = 0; i < productBlocks.length; i++) {
+			const idSelector = await page.$eval(`.item-card:nth-of-type(${i + 1})`, prod => {
+				return prod.getAttribute('data-product-id')
+			})
+			const nameSelector = await productBlocks[i].$('.item-card__name-link')
+			const priceSelector = await productBlocks[i].$('.item-card__prices-price')
+			const linkSelector = await productBlocks[i].$('.item-card__image-wrapper')
+			const imageSelector = await productBlocks[i].$('.item-card__image')
+
+			const productInfo = {
+				id: idSelector,
+				name: await getInnerText(nameSelector),
+				price: trimBoth(removeNewLineFromString(await getInnerText(priceSelector))),
+				url: await getHref(linkSelector),
+				imgLink: await getSrc(imageSelector),
+			}
+
+			products.push(productInfo)
+		}
 
 		await browser.close()
 
