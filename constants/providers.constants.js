db674e6925ae3930c945e98d2b4b8d0d8693b505
Maksat Kuanysh <boktaban@gmail.com>
2023-05-14 11:54:05 +0600

    feat: add constants

--- constants/providers.constants.js
+++ constants/providers.constants.js
@@ -1,8 +1,8 @@
 // need to test another urls
 export const KAZAKHTELECOM = 'kazakhtelecom' // (https://consumer.oofd.kz/ticket/653bf166-a399-4260-9557-4f6eaf3682b8)
-export const TTC = 'transtelecom' // (https://ofd1.kz/t/?i=56689177603&f=010101907419&s=2508.0&t=20221216T152000)
+export const TRANSTELECOM = 'transtelecom' // (https://ofd1.kz/t/?i=56689177603&f=010101907419&s=2508.0&t=20221216T152000)
 
 export const providers = {
-	'ofd1.kz': TTC,
+	'ofd1.kz': TRANSTELECOM,
 	'consumer.oofd.kz': KAZAKHTELECOM,
 }
